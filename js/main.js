74ff3e8adcfb4d0612045386aa86c1090379ca1c
panbo <362472407@qq.com>
2024-10-08 12:55:43 +0800

    [feat] 1、增加pricing section 2、学习基础的css语法

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// faq accordion
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContent = document.querySelector('.faq-content');
+
+    faqContent.addEventListener('click', (e) => {
+
+        // get the closest faq-group-header element
+        const faqGroupHeader = e.target.closest('.faq-group-header');
+
+        // if the clicked element is not a faq-group-header, return
+        if (!faqGroupHeader) return;
+
+        // get the faq-group-body element
+        const faqGroup = faqGroupHeader.parentElement;
+        const faqGroupBody = faqGroup.querySelector('.faq-group-body');
+        const icon = faqGroupHeader.querySelector('i');
+
+        // toggle the icon
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        // Toggle visibility of the faq-group-body element
+        faqGroupBody.classList.toggle('open');
+
+        // 找到其他的 faq-group-body 并设置为关闭
+        const faqGroups = faqContent.querySelectorAll('.faq-group');
+        faqGroups.forEach((group) => {
+            if (group !== faqGroup) {
+                group.querySelector('.faq-group-body').classList.remove('open');
+                group.querySelector('i').classList.remove('fa-minus');
+                group.querySelector('i').classList.add('fa-plus');
+            }
+        });
+
+    });
+});
+
+
+//mobile menu
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+    });
+
+});
